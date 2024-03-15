1b551262469c546e74db3c80df9700d0be0236f5
jay <jay@bytedance.com>
2024-03-15 18:07:24 +0800

    update filter rules on mr titles to migrate internal mrs to external

--- utils/create_pr/create_pr.sh
+++ utils/create_pr/create_pr.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-source $(dirname "$0")/proc_title.sh
-
-if [ -z "$2" ]; then
-    BRANCH=$(git branch --show-current)
-else
-    BRANCH=$2
-fi
+source ./proc_title.sh
 
 if [ -z "$1" ]; then
-    TARGET_BRANCH=master
+    BRANCH=$(git branch --show-current)
 else
-    TARGET_BRANCH=$1
+    BRANCH=$1
 fi
-
 
 GIT_LOG=$(git log refs/heads/$BRANCH^! | tail -n +4)
 TITLE=$(process_header "$GIT_LOG")
 
 echo "create pr with title: ${TITLE} for branch ${BRANCH}"
-gh pr create --base "${TARGET_BRANCH}" --head "$BRANCH" --title "$TITLE" --body "$TITLE"
+gh pr create --base master --head $BRANCH --title "$TITLE" --body "$TITLE"
 
