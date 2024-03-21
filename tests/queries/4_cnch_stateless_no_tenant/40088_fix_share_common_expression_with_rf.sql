ba5fb2d6970303db30f6c259afc1c6d7c3ea3447
Dao <minh.dao@bytedance.com>
2024-03-21 11:45:20 +0800

    Merge 'fix_share_common_expression_with_rf_cnch-dev' into 'cnch-dev'
    fix(clickhousech@m-3011587410): fix share common expression with rf cnch dev
    
    See merge request: https://code.byted.org/dp/ClickHouse/merge_requests/20099

--- tests/queries/4_cnch_stateless_no_tenant/40088_fix_share_common_expression_with_rf.sql
+++ tests/queries/4_cnch_stateless_no_tenant/40088_fix_share_common_expression_with_rf.sql
@@ -16,22 +16,22 @@ INSERT INTO t40088_2 SELECT number FROM system.numbers LIMIT 10;
 CREATE STATS t40088_1 FORMAT Null;
 CREATE STATS t40088_2 FORMAT Null;
 
--- EXPLAIN stats = 0
--- SELECT count(*)
--- FROM
--- (
---     SELECT toInt32(((a + 1) + 1) + 1) AS k
---     FROM t40088_1
---     WHERE k > 1
--- ) AS r
--- INNER JOIN t40088_2 AS s ON r.k = s.x
--- SETTINGS
---   enable_common_expression_sharing = 1,
---   common_expression_sharing_threshold = 1,
---   enable_runtime_filter = 1,
---   runtime_filter_min_filter_rows = 1,
---   runtime_filter_min_filter_factor = 0.001,
---   enum_replicate = 0;
+EXPLAIN stats = 0
+SELECT count(*)
+FROM
+(
+    SELECT toInt32(((a + 1) + 1) + 1) AS k
+    FROM t40088_1
+    WHERE k > 1
+) AS r
+INNER JOIN t40088_2 AS s ON r.k = s.x
+SETTINGS
+  enable_common_expression_sharing = 1,
+  common_expression_sharing_threshold = 1,
+  enable_runtime_filter = 1,
+  runtime_filter_min_filter_rows = 1,
+  runtime_filter_min_filter_factor = 0.001,
+  enum_replicate = 0;
 
 SELECT count(*)
 FROM
