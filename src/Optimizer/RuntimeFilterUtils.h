9f713a1dd181ece0f2caf451cd886a3640a6c3c9
jay <jay@bytedance.com>
2024-04-06 08:06:19 +0800

    Merge 'fix_share_common_expression_with_rf_cnch-2.1' into 'cnch-2.1'
    fix(clickhousech@m-3011587410): fix share common expression with rf cnch 2.1
    
    See merge request: https://code.byted.org/dp/ClickHouse/merge_requests/20100

--- src/Optimizer/RuntimeFilterUtils.h
+++ src/Optimizer/RuntimeFilterUtils.h
@@ -11,15 +11,14 @@ namespace DB
 {
 struct RuntimeFilterDescription
 {
-    RuntimeFilterDescription(RuntimeFilterId id_, ASTPtr expr_, double filter_factor_, std::vector<ASTPtr> partition_columns_exprs_)
-        : id(id_), expr(std::move(expr_)), filter_factor(filter_factor_), partition_columns_exprs(std::move(partition_columns_exprs_))
+    RuntimeFilterDescription(RuntimeFilterId id_, ConstASTPtr expr_, double filter_factor_)
+        : id(id_), expr(std::move(expr_)), filter_factor(filter_factor_)
     {
     }
 
     RuntimeFilterId id;
-    ASTPtr expr;
+    ConstASTPtr expr;
     double filter_factor;
-    std::vector<ASTPtr> partition_columns_exprs;
 };
 
 class SymbolStatistics;
@@ -28,11 +27,24 @@ class PlanNodeStatistics;
 using PlanNodeStatisticsPtr = std::shared_ptr<PlanNodeStatistics>;
 class FilterStep;
 
+/**
+ * Runtime Filter, also as runtime filter, improve the performance of queries with selective joins
+ * by filtering data early that would be filtered by join condition.
+ *
+ * When runtime Filtering is enabled, values are collected from the build side of join, and sent to
+ * probe side of join in runtime.
+ *
+ * Runtime Filter could be used for dynamic partition pruning, reduce table scan data with index,
+ * reduce exchange shuffle, and so on.
+ *
+ * Runtime Filter has two parts in plan:
+ *  1. build side, model as projection attribute.
+ *  2. consumer side, model as a filter predicates.
+ */
 class RuntimeFilterUtils
 {
 public:
-    static ConstASTPtr createRuntimeFilterExpression(
-        RuntimeFilterId id, const std::string & symbol, const std::vector<String> & partition_columns, double filter_factor);
+    static ConstASTPtr createRuntimeFilterExpression(RuntimeFilterId id, const std::string & symbol);
     static ConstASTPtr createRuntimeFilterExpression(const RuntimeFilterDescription & description);
 
     static RuntimeFilterId extractId(const ConstASTPtr & runtime_filter);
@@ -40,27 +52,22 @@ public:
 
     /* runtime_filters, static_filters */
     static std::pair<std::vector<ConstASTPtr>, std::vector<ConstASTPtr>> extractRuntimeFilters(const ConstASTPtr & conjuncts);
-    static std::pair<std::vector<ConstASTPtr>, std::vector<ConstASTPtr>> extractExecutableRuntimeFiltersAndPush1stRf(const ConstASTPtr & conjuncts);
-    static std::pair<std::vector<ConstASTPtr>, std::vector<ConstASTPtr>> extractExecutableRuntimeFilters(const ConstASTPtr & conjuncts);
     static bool isInternalRuntimeFilter(const ConstASTPtr & expr);
     static bool isInternalRuntimeFilter(const ASTFunction & function);
-    static bool isExecutableRuntimeFilter(const ConstASTPtr & expr);
 
     static double estimateSelectivity(
-        const ASTPtr & expr,
-        const SymbolStatisticsPtr & build_stats,
-        const PlanNodeStatisticsPtr & probe_stats,
-        const NamesAndTypes & column_types,
+        const RuntimeFilterDescription & description,
+        const SymbolStatisticsPtr & filter_source,
+        const PlanNodeStatisticsPtr & child_stats,
+        const FilterStep & step,
         ContextMutablePtr & context);
 
     static std::vector<ASTPtr>
-    createRuntimeFilterForFilter(const RuntimeFilterDescription & description, const String & query_id, bool only_bf);
+    createRuntimeFilterForFilter(const RuntimeFilterDescription & description, const String & query_id);
 
-    // RuntimeFilterBloomFilterExists will be at the end of std::vector<ASTPtr> .
     static std::vector<ASTPtr> createRuntimeFilterForTableScan(
-        const RuntimeFilterDescription & description, const String & query_id, size_t wait_ms, bool need_bf, bool range_cover, bool & is_range_or_set, bool & has_bf);
+        const RuntimeFilterDescription & description, const String & query_id, size_t wait_ms, bool need_bf, bool range_cover);
 
-    static std::vector<ASTPtr> generateFunctionArgs(const RuntimeFilterDescription & description, const String & query_id);
     static bool containsRuntimeFilters(const ConstASTPtr & filter);
     static std::vector<RuntimeFilterId> extractRuntimeFilterId(const ConstASTPtr & conjuncts);
 };
