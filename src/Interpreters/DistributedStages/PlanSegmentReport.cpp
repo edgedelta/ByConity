0f674b67cd8343148283dc5a2ebb3f071b0d760b
fredwang <fredwang@bytedance.com>
2024-07-17 17:38:48 +0000

    Merge branch 'rpc_channel_pool_dev' into 'cnch-dev'
    feat(clickhousech@m-3892754039): 【CP】optimize rpc_channel_pool
    
    See merge request dp/ClickHouse!21807

--- src/Interpreters/DistributedStages/PlanSegmentReport.cpp
+++ src/Interpreters/DistributedStages/PlanSegmentReport.cpp
@@ -22,7 +22,7 @@ void reportPlanSegmentStatus(
         auto address = extractExchangeHostPort(coordinator_address);
 
         std::shared_ptr<RpcClient> rpc_client
-            = RpcChannelPool::getInstance().getClient(address, BrpcChannelPoolOptions::DEFAULT_CONFIG_KEY, true);
+            = RpcChannelPool::getInstance().getClient(address, BrpcChannelPoolOptions::DEFAULT_CONFIG_KEY);
         Protos::PlanSegmentManagerService_Stub manager(&rpc_client->getChannel());
         brpc::Controller cntl;
         Protos::SendPlanSegmentStatusRequest request;
--- src/Processors/Executors/PipelineExecutor.cpp
+++ src/Processors/Executors/PipelineExecutor.cpp
@@ -530,7 +530,7 @@ void reportToCoordinator(
     try
     {
         auto address = extractExchangeHostPort(coordinator_address);
-        std::shared_ptr<RpcClient> rpc_client = RpcChannelPool::getInstance().getClient(address, BrpcChannelPoolOptions::DEFAULT_CONFIG_KEY, true);
+        std::shared_ptr<RpcClient> rpc_client = RpcChannelPool::getInstance().getClient(address, BrpcChannelPoolOptions::DEFAULT_CONFIG_KEY);
         Protos::PlanSegmentManagerService_Stub manager(&rpc_client->getChannel());
         brpc::Controller cntl;
         Protos::ReportProcessorProfileMetricRequest request;
@@ -557,7 +557,7 @@ void reportToCoordinator(
     try
     {
         auto address = extractExchangeHostPort(coordinator_address);
-        std::shared_ptr<RpcClient> rpc_client = RpcChannelPool::getInstance().getClient(address, BrpcChannelPoolOptions::DEFAULT_CONFIG_KEY, true);
+        std::shared_ptr<RpcClient> rpc_client = RpcChannelPool::getInstance().getClient(address, BrpcChannelPoolOptions::DEFAULT_CONFIG_KEY);
         Protos::PlanSegmentManagerService_Stub manager(&rpc_client->getChannel());
         brpc::Controller cntl;
         Protos::BatchReportProcessorProfileMetricRequest requests;
